e2e9e7b46dc02f6ccfd3a6bc2db40a93178bcc72
Samlv9 <samlv9@live.com>
2016-01-23 11:19:04 +0800

    4.0.2

--- src/Integralmall/src/view/WeToast.js
+++ src/Integralmall/src/view/WeToast.js
@@ -27,7 +27,7 @@
 /// THE SOFTWARE.
 
 
-var WE_TOAST_TEMPLATE  = '<div class="weui_loading_toast">'
+var WE_TOAST_TEMPLATE  = '<div class="{{IS_LOADING_TOAST}}">'
     + '<div class="weui_mask_transparent"></div>'
     + '<div class="weui_toast">'
     + '{{TOAST_LOADING}}'
@@ -50,14 +50,18 @@ var WE_TOAST_LOADING = '<div class="weui_loading">'
     + '<div class="weui_loading_leaf weui_loading_leaf_11"></div>'
     + '</div>';
 
+var WE_TOAST_ICON = '<i class="weui_icon_toast"></i>';
+
 
 var WE_TOAST_ELEMENT = null;
 var WE_TOAST_INTERVAL = 0;
+var WE_DONE_TOAST_ELEMENT = null;
+var WE_DONE_TOAST_INTERVAL = 0;
 
 
 function ShowWeToast( textContent, autoRemove ) {
     if ( !WE_TOAST_ELEMENT ) {
-        WE_TOAST_ELEMENT = CreateToastElement();
+        WE_TOAST_ELEMENT = CreateToastElement('weui_loading_toast');
     }
 
     textContent = textContent || '请稍后...';
@@ -65,24 +69,48 @@ function ShowWeToast( textContent, autoRemove ) {
     document.body.appendChild(WE_TOAST_ELEMENT);
 
     if ( autoRemove && autoRemove >= 100 ) {
+        clearTimeout(WE_TOAST_INTERVAL);
         WE_TOAST_INTERVAL = setTimeout(HideWeToast, autoRemove);
     }
 }
 
 
-function HideWeToast() {
-    clearTimeout(WE_TOAST_INTERVAL);
+function ShowWeToastDone( textContent, autoRemove ) {
+    if ( !WE_DONE_TOAST_ELEMENT ) {
+        WE_DONE_TOAST_ELEMENT = CreateToastElement('');
+    }
 
+    textContent = textContent || '已完成';
+    WE_DONE_TOAST_ELEMENT.querySelector('.weui_toast_content').textContent = textContent;
+    document.body.appendChild(WE_DONE_TOAST_ELEMENT);
+
+    if ( autoRemove && autoRemove >= 100 ) {
+        clearTimeout(WE_DONE_TOAST_INTERVAL);
+        WE_DONE_TOAST_INTERVAL = setTimeout(HideWeToast, autoRemove);
+    }
+}
+
+
+function HideWeToast() {
     if ( WE_TOAST_ELEMENT ) {
         document.body.removeChild(WE_TOAST_ELEMENT);
     }
 }
 
+function HideWeToastDone() {
+    if ( WE_DONE_TOAST_ELEMENT ) {
+        document.body.removeChild(WE_DONE_TOAST_ELEMENT);
+    }
+}
 
-function CreateToastElement() {
+
+function CreateToastElement( isLoadingToast ) {
     var html = WE_TOAST_TEMPLATE.replace('{{TOAST_LOADING}}', WE_TOAST_LOADING);
+        html = html.replace('{{IS_LOADING_TOAST}}', isLoadingToast);
     var div = document.createElement("div");
         div.innerHTML = html;
 
     return div.firstElementChild;
 }
+
+
